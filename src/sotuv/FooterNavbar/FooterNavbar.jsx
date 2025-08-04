a1277a4c22ad49f3965760eb19cb9d0a3ada3796
Xojiakbar-vscode <qodirjanovxojiakbar0@gmail.com>
2025-08-04 14:10:43 +0500

    Tailor

--- src/sotuv/FooterNavbar/FooterNavbar.jsx
+++ src/sotuv/FooterNavbar/FooterNavbar.jsx
@@ -1,59 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./FooterNavbar.css";
-import Like from "../../images/Live.png"
-import Logo from "../../images/Logo.png"
-import Savat from "../../images/Savat.png"
-import Katalog from "../../images/Katalog.png"
-import User from "../../images/user.png"
-import { Link } from 'react-router-dom';
 
-import CatalogModal from '../CatalogModal/CatalogModal';
-import CartModal from '../CartModal/CartModal';
+import Like from "../../images/Live.png";
+import Logo from "../../images/Logo.png";
+import Savat from "../../images/Savat.png";
+import Katalog from "../../images/Katalog.png";
+import User from "../../images/user.png";
+
+import { Link } from "react-router-dom";
+import CatalogModal from "../CatalogModal/CatalogModal";
+import CartModal from "../CartModal/CartModal";
 
 const FooterNavbar = () => {
   const [showCatalogModal, setShowCatalogModal] = useState(false);
-    const [showCartModal, setShowCartModal] = useState(false);
+  const [showCartModal, setShowCartModal] = useState(false);
+
   return (
     <div className="footer-navbar">
-  <Link to="/">
-      <div>
-    
-          <img src={Logo} alt="" />
+      {/* Bosh sahifa */}
+      <Link to="/" onClick={() => window.scrollTo(0, 0)}>
+        <div>
+          <img src={Logo} alt="Bosh sahifa" />
           <p>Sahifaga qaytish</p>
-      </div>
-  </Link>
+        </div>
+      </Link>
 
-      <div >
-        <img
-          src={Savat}
-          alt=""
-         onClick={() => setShowCartModal(true)}
-        />
+      {/* Buyurtmalar */}
+      <div onClick={() => setShowCartModal(true)} style={{ cursor: "pointer" }}>
+        <img src={Savat} alt="Buyurtmalar" />
         <p>Buyurtmalar</p>
       </div>
 
+      {/* Yoqtirganlarim */}
       <div>
-        <img src={Like} alt="" />
+        <img src={Like} alt="Yoqtirganlarim" />
         <p>Yoqtirganlarim</p>
       </div>
 
-      <div className="katalog catalog-trigger-1"  onClick={() => setShowCatalogModal(true)}>
-        <img src={Katalog} alt="" />
+      {/* Katalog */}
+      <div
+        className="katalog catalog-trigger-1"
+        onClick={() => setShowCatalogModal(true)}
+        style={{ cursor: "pointer" }}
+      >
+        <img src={Katalog} alt="Katalog" />
         <p>Katalog</p>
       </div>
 
+      {/* Profil */}
       <div>
-        <img src={User} alt="" />
+        <img src={User} alt="Profil" />
         <p>Profil</p>
       </div>
-       <CatalogModal 
-        show={showCatalogModal} 
-        onHide={() => setShowCatalogModal(false)} 
+
+      {/* Modallar */}
+      <CatalogModal
+        show={showCatalogModal}
+        onHide={() => setShowCatalogModal(false)}
       />
-      <CartModal 
-        show={showCartModal} 
-        onHide={() => setShowCartModal(false)} 
+      <CartModal
+        show={showCartModal}
+        onHide={() => setShowCartModal(false)}
       />
     </div>
   );
