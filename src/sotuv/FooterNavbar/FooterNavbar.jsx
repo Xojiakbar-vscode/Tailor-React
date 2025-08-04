b76a3fa4c6b5746ce3d980a993f1eb1468058dbf
Xojiakbar-vscode <qodirjanovxojiakbar0@gmail.com>
2025-08-04 14:00:17 +0500

    Tailor

--- src/sotuv/FooterNavbar/FooterNavbar.jsx
+++ src/sotuv/FooterNavbar/FooterNavbar.jsx
@@ -6,25 +6,29 @@ import Logo from "../../images/Logo.png"
 import Savat from "../../images/Savat.png"
 import Katalog from "../../images/Katalog.png"
 import User from "../../images/user.png"
+import { Link } from 'react-router-dom';
 
+import CatalogModal from '../CatalogModal/CatalogModal';
+import CartModal from '../CartModal/CartModal';
 
 const FooterNavbar = () => {
+  const [showCatalogModal, setShowCatalogModal] = useState(false);
+    const [showCartModal, setShowCartModal] = useState(false);
   return (
     <div className="footer-navbar">
+  <Link to="/">
       <div>
-      
+    
           <img src={Logo} alt="" />
           <p>Sahifaga qaytish</p>
-  
       </div>
+  </Link>
 
-      <div>
+      <div >
         <img
           src={Savat}
           alt=""
-          data-bs-toggle="modal"
-          data-bs-target="#cartModal"
-          style={{ cursor: "pointer" }}
+         onClick={() => setShowCartModal(true)}
         />
         <p>Buyurtmalar</p>
       </div>
@@ -34,7 +38,7 @@ const FooterNavbar = () => {
         <p>Yoqtirganlarim</p>
       </div>
 
-      <div className="katalog catalog-trigger-1" id="catalogTrigger1">
+      <div className="katalog catalog-trigger-1"  onClick={() => setShowCatalogModal(true)}>
         <img src={Katalog} alt="" />
         <p>Katalog</p>
       </div>
@@ -43,6 +47,14 @@ const FooterNavbar = () => {
         <img src={User} alt="" />
         <p>Profil</p>
       </div>
+       <CatalogModal 
+        show={showCatalogModal} 
+        onHide={() => setShowCatalogModal(false)} 
+      />
+      <CartModal 
+        show={showCartModal} 
+        onHide={() => setShowCartModal(false)} 
+      />
     </div>
   );
 };
